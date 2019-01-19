29f2ca890830bd77a96808b5e8cb7e751a61821b
oswin <uchwatm@gmail.com>
2019-01-19 19:18:54 +0100

    addFlyingBox

--- Assets/main.cs
+++ Assets/main.cs
@@ -1,30 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class main : MonoBehaviour {
+	public float start_x = 0.0f;
+	public float start_y = 8.2f;
+	public float start_z = 0.0f;
 
 	public GameObject multiTarget;
 	public GameObject plane;
-	public InputHelper inputHelper;
+	//public InputHelper inputHelper;
 	public List<Touch> touches;
+	public Transform box;
+	public q current;
+	
+	public q AddFlyingCube(){
+		Transform transformObj = Instantiate(box, new Vector3(), Quaternion.identity);
+		GameObject clone = transformObj.gameObject;
+		q qObj = clone.GetComponent<q>();
+		qObj.transform.parent = this.transform;
+		qObj.transform.localPosition = new Vector3(start_x, start_y, start_z);
+		return qObj;
+	}
 	
 
 	// Use this for initialization
 	void Start () {
-		
-		inputHelper = new InputHelper();
-
-		GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
-		foreach(GameObject obj in allObjects){
-			print("TEST:\t" + obj.name)  ;
-		}
-		 plane = GameObject.Find("Plane");
-		 print("Plane kids:\t" + plane.transform.childCount);
-		 
-		 
-		 
+		current = AddFlyingCube(); 
+		print("Current position:\t"+current.transform.position);
 	}
 	
 	// Update is called once per frame
@@ -37,16 +40,12 @@ public class main : MonoBehaviour {
 
 		foreach(Touch touch in touches) {
 			if (touch.phase == TouchPhase.Began) {
-
-				//make all plane children fall
-				foreach (Transform child in plane.transform){
-					print(child.transform);
-					print(child.parent);
+				current.Fall();
+				current = AddFlyingCube();					
 					
-					
-				}
-				
 			}
+				
 		}
 	}
+	
 }
