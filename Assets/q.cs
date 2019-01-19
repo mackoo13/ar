80926ac0354482de7123ae0d87062017b6e7d96c
oswin <uchwatm@gmail.com>
2019-01-19 18:53:09 +0100

    modify q

--- Assets/q.cs
+++ Assets/q.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class q : MonoBehaviour {
+	public float start_x = 0.0f;
+	public float start_y = 8.2f;
+	public float start_z = 8.2f;
 	
 	Rigidbody rb;
 	public bool falling;
@@ -11,23 +14,26 @@ public class q : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		falling = false;
+		transform.localPosition = new Vector3(start_x, start_y, start_z);
+	}
+	
+	public void Test(){
+		print("Tralalala");
+	}
+	
+	public void Fall(){
+		print("FALLING!");
+		falling = true;
+		rb.useGravity = true;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(!falling) {
 			float t = Time.realtimeSinceStartup;
-			transform.position = new Vector3(Mathf.Sin(t*2) * 5, transform.position[1], transform.position[2]);
-		}
-		else{
-			print(transform.position);
-		}
-		
-		foreach(Touch touch in Input.touches) {
-			if (touch.phase == TouchPhase.Began) {
-				falling = true;
-				rb.useGravity = true;
-			}
+			transform.localPosition = new Vector3(Mathf.Sin(t*2) * 5, transform.localPosition[1], transform.localPosition[2]);
 		}
 	}
+
 }
